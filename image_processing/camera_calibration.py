96471a5a13803ee7358be1047414a8eeec00184a
mmenuu <rctkhtp@gmail.com>
2023-06-14 19:19:53 +0700

    fix(calibration): add saving

--- image_processing/camera_calibration.py
+++ image_processing/camera_calibration.py
@@ -15,6 +15,9 @@ cap = cv2.VideoCapture(0)
 # Define termination criteria for the iterative algorithm
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
+# Counter for image filenames
+counter = 0
+
 while True:
     # Read a frame from the camera
     ret, frame = cap.read()
@@ -32,6 +35,9 @@ while True:
         image_points_list.append(corners)
         object_points_list.append(object_points)
 
+    # Save the frame before calibration
+    cv2.imwrite(f'before_calibration_{counter}.png', frame)
+
     # Display the frame
     cv2.imshow('Camera Calibration', frame)
 
@@ -39,13 +45,39 @@ while True:
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Release the capture object and destroy windows
-cap.release()
-cv2.destroyAllWindows()
+    # Increment the counter
+    counter += 1
 
 # Perform camera calibration
 ret, camera_matrix, distortion_coefficients, rotation_vectors, translation_vectors = cv2.calibrateCamera(
     object_points_list, image_points_list, gray.shape[::-1], None, None)
+
+while True:
+    # Read a frame from the camera
+    ret, frame = cap.read()
+
+    # Convert the frame to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Undistort the frame
+    undistorted_frame = cv2.undistort(frame, camera_matrix, distortion_coefficients)
+
+    # Save the frame after calibration
+    cv2.imwrite(f'after_calibration_{counter}.png', undistorted_frame)
+
+    # Display the undistorted frame
+    cv2.imshow('Camera Calibration (Undistorted)', undistorted_frame)
+
+    # Exit the loop if 'q' is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+    # Increment the counter
+    counter += 1
+
+# Release the capture object and destroy windows
+cap.release()
+cv2.destroyAllWindows()
 
 # Print the calibration results
 print("Camera matrix:")
